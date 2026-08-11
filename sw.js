87a5b5a4aca09734556ed30147796fabe5afb7b5
quiet-axis2024 <jinx.lin0820@gmail.com>
2026-08-12 02:49:17 +0800

    Bump service worker cache to v30

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='stardew-tracker-v29';
+const CACHE='stardew-tracker-v30';
 const CORE=['./index.html','./app.js','./cloud.js','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',event=>{
