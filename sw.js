1173296f4a9b6e331dc56d0916bccbd4a41b59c8
quiet-axis2024 <jinx.lin0820@gmail.com>
2026-08-11 21:55:37 +0800

    Bump PWA cache to v15 after runtime repair

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='stardew-tracker-v14';
+const CACHE='stardew-tracker-v15';
 const CORE=['./index.html','./app.js','./cloud.js','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',event=>{
