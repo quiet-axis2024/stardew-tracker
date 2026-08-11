d6bf70eeb893de2532771d82fd9e4992fbdee893
quiet-axis2024 <jinx.lin0820@gmail.com>
2026-08-11 19:32:17 +0800

    Refresh PWA cache for cooking collection update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='stardew-tracker-v10';
+const CACHE='stardew-tracker-v11';
 const CORE=['./index.html','./app.js','./cloud.js','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',event=>{
